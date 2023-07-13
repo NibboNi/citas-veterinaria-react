d725e02728947298419b3eb43f29501a46a53a05
NibboNi <131214845+NibboNi@users.noreply.github.com>
2023-07-13 13:49:32 -0600

    created Paciente component

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,12 @@
+function Paciente() {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+        <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: <span className="font-normal normal-case">Hook</span></p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: <span className="font-normal normal-case">Nico</span></p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Email: <span className="font-normal normal-case">correo@correo.com</span></p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta: <span className="font-normal normal-case">10 Diciembre 2023</span></p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas: <span className="font-normal normal-case">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et ea neque, explicabo error nulla praesentium id necessitatibus laborum fuga nihil, officiis sint mollitia, illo reprehenderit. Nostrum officia exercitationem eaque hic.</span></p>
+    </div>
+  )
+}
+export default Paciente
